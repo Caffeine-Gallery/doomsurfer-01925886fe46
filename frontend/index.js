f9532a11b81425674021b767c6f8b134b9880a62
sa-gems-github <sa-gems-github@dfinity.org>
2024-10-04 16:00:51 -0400

    0192591e-a7b0-766e-8bba-8c4970ef97b2

--- frontend/index.js
+++ frontend/index.js
@@ -4,9 +4,9 @@ let dosBox;
 const JS_DOS_VERSION = '6.22';
 const CDN_URLS = [
     {
-        js: `https://js-dos.com/${JS_DOS_VERSION}/js-dos.js`,
-        wdosbox: `https://js-dos.com/${JS_DOS_VERSION}/dosbox.js`,
-        wdosboxWasm: `https://js-dos.com/${JS_DOS_VERSION}/wdosbox.wasm`
+        js: `https://js-dos.com/${JS_DOS_VERSION}/current/js-dos.js`,
+        wdosbox: `https://js-dos.com/${JS_DOS_VERSION}/current/dosbox.js`,
+        wdosboxWasm: `https://js-dos.com/${JS_DOS_VERSION}/current/wdosbox.wasm`
     },
     {
         js: `https://cdn.jsdelivr.net/npm/js-dos@${JS_DOS_VERSION}/dist/js-dos.js`,
